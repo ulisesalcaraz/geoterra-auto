88acc163360798f42f9568214fcf87453becad15
Ulises Alcaraz <51634994+ulisesalcaraz@users.noreply.github.com>
2023-03-07 16:46:53 -0300

    npm init

---
+++ index.js
@@ -0,0 +1,15 @@
+// create clients data
+
+// fetch api requests
+
+// process data into information
+
+// save images into files
+
+// generate pdf
+
+// save pdf on google drive
+
+// send notification message
+
+// clean generated files
